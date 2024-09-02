96eb3da73ff0323be09e8060f2740da50b26b6b2
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-09-02 19:06:06 +0000

    Banco de dados implementado no Read By ID

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const dbUrl = 'mongodb+srv://' + dbUser + ':' + dbPassword +'@cluster0.zaxp3ht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const dbName = 'mongodb-intro-e-implementacao'
@@ -33,12 +33,12 @@ async function main() {
   })
 
   // Endpoint Read by ID (GET) /personagem/:id
-  app.get('/personagem/:id', function (req, res) {
+  app.get('/personagem/:id', async function (req, res) {
     // Acessamos o parâmetro de rota ID
     const id = req.params.id
 
-    // Acessa o item na lista usando o ID -1
-    const item = lista[id - 1]
+    // Acessa o item na collection usando o ID
+    const item = await collection.findOne({ _id: new ObjectId(id) })
 
     // Checamos se o item existe
     if (!item) {
